8433fb7904b2de81db67b5a66430ed01ed15bd60
edwinfred <61155818+edwinfred@users.noreply.github.com>
2022-04-27 16:13:05 +0530

    Changed test string

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main(){
 	      			fmt.Println(err)
 	   			}
 	   			sb := string(body)
-	   			check_result := strings.Contains(sb , "alert(1)")
+	   			check_result := strings.Contains(sb , "<img src=x onerror=alert(1)>")
 	   			// fmt.Println(check_result)
 	   			if check_result != false {
 	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
